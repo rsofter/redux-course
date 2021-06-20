921b9594cc3f21c7b3a98696fd9475298df150b6
Alexander Matlin <ratingsoft@gmail.com>
2021-06-20 16:46:26 +0200

    Add "bugs-frontend" folder.

---
+++ bugs-frontend/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import configureStore from './store/configureStore';
+import { Provider } from 'react-redux';
+import BugsList from './components/BugsList';
+
+const store = configureStore();
+
+function App() {
+	return (
+		<Provider store={store}>
+			<BugsList />
+		</Provider>
+	);
+}
+
+export default App;
---
+++ bugs-frontend/src/components/BugsList.jsx
@@ -0,0 +1,22 @@
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { loadBugs, resolveBug, getUnresolvedBugs } from '../store/bugs';
+
+const BugsList = () => {
+	const dispatch = useDispatch();
+	const bugs = useSelector(getUnresolvedBugs);
+
+	useEffect(() => {
+		dispatch(loadBugs());
+	}, []);
+
+	return (
+		<ul>
+			{bugs.map(bug => <li key={bug.id}>
+				{bug.description} <button onClick={() => dispatch(resolveBug(bug.id))}>Resolve</button>
+			</li>)}
+		</ul>
+	);
+};
+
+export default BugsList;
